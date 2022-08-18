06491d6359fb7e488ff8a5332a9ae9d6246f650c
Marcel Joschko <marceljoschko@proton.me>
2022-08-18 18:14:01 +0200

    removed css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, ThemeProvider } from "theme-ui";
+import { jsx } from "theme-ui";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import theme from "./theme";
 import App from "./app";
 import { Provider } from "./app-state";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<Provider>
-				<App />
-			</Provider>
-		</ThemeProvider>
+		<Provider>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
